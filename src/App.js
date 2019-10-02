3e8ca9bcf93f0e090dec60db58f38438192873d7
Thais Laney <laney.thais@gmail.com>
2019-10-02 11:36:32 -0400

    unique box keys

--- src/App.js
+++ src/App.js
@@ -6,44 +6,58 @@ import Box from './Box';
 import './App.css';
 
 const App = () => {
+  let keyCount = 0;
   const spacing = 1.1;
-  const boxes = [<Box key={0} initialPosition={[0, 0, 0]} />];
-  for (var x = spacing; x <= 9 * spacing; x += spacing) {
-    for (var z = spacing; z <= 5 * spacing; z += spacing) {
+  const boxes = [<Box key={`box#${keyCount++}`} initialPosition={[0, 0, 0]} />];
+  for (var x = spacing; x <= 10 * spacing; x += spacing) {
+    for (var z = spacing; z <= 6 * spacing; z += spacing) {
       if (Math.random() >= 0.7) {
-        boxes.push(<Box key={`box${x},0,${z}`} initialPosition={[x, 0, z]} />);
         boxes.push(
-          <Box key={`box${-x},0,${z}`} initialPosition={[-x, 0, z]} />
+          <Box key={`box#${keyCount++}`} initialPosition={[x, 0, z]} />
         );
         boxes.push(
-          <Box key={`box${x},0,${-z}`} initialPosition={[x, 0, -z]} />
+          <Box key={`box#${keyCount++}`} initialPosition={[-x, 0, z]} />
         );
         boxes.push(
-          <Box key={`box${-x},${-z}`} initialPosition={[-x, 0, -z]} />
+          <Box key={`box#${keyCount++}`} initialPosition={[x, 0, -z]} />
+        );
+        boxes.push(
+          <Box key={`box#${keyCount++}`} initialPosition={[-x, 0, -z]} />
         );
       }
 
       if (Math.random() >= 0.9) {
-        boxes.push(<Box key={`box0,0,${z}`} initialPosition={[0, 0, z]} />);
+        boxes.push(
+          <Box key={`box#${keyCount++}`} initialPosition={[0, 0, z]} />
+        );
       }
 
       if (Math.random() >= 0.9) {
-        boxes.push(<Box key={`box0,0,${-z}`} initialPosition={[0, 0, -z]} />);
+        boxes.push(
+          <Box key={`box#${keyCount++}`} initialPosition={[0, 0, -z]} />
+        );
       }
 
       if (Math.random() >= 0.9) {
-        boxes.push(<Box key={`box${x},0,0`} initialPosition={[x, 0, 0]} />);
+        boxes.push(
+          <Box key={`box#${keyCount++}`} initialPosition={[x, 0, 0]} />
+        );
       }
 
       if (Math.random() >= 0.9) {
-        boxes.push(<Box key={`box${-x},0,0`} initialPosition={[-x, 0, 0]} />);
+        boxes.push(
+          <Box key={`box#${keyCount++}`} initialPosition={[-x, 0, 0]} />
+        );
       }
     }
   }
 
   return (
     <div className="App">
-      <Canvas camera={{ position: [0, 4, 10], lookAt: [0, 0, 0] }}>
+      <Canvas
+        style={{ touchAction: 'none' }}
+        camera={{ position: [0, 4, 10], lookAt: [0, 0, 0] }}
+      >
         <ambientLight />
         <spotLight position={[0, 8, 5]} />
         {boxes}
