f6b6a0aa7e780e84cdcd09c410fecc4fbaa9061b
Thais Laney <laney.thais@gmail.com>
2019-10-03 13:06:32 -0400

    a house to start

--- src/App.js
+++ src/App.js
@@ -1,60 +1,35 @@
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
-
-import Box from './Box';
-
+import React, { useRef } from 'react';
+import { Canvas, extend, useThree, useRender } from 'react-three-fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './App.css';
-import { hsluvToHex } from 'hsluv';
+import House from './House';
 
-const hueMin = 260;
-const hueMax = 340;
-const satMin = 60;
-const satMax = 90;
-const spacing = 1.1;
-const hueStep = (hueMax - hueMin) / (10 * spacing);
-const randColor = () =>
-  hsluvToHex([
-    Math.random() * (hueMax - hueMin) + hueMin,
-    Math.random() * (satMax - satMin) + satMin,
-    72
-  ]);
+extend({ OrbitControls });
 
-const xBasedColor = x =>
-  hsluvToHex([
-    x === 0 ? hueMax : hueMax - x * 6,
-    Math.random() * (satMax - satMin) + satMin,
-    72
-  ]);
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+
+  useRender(() => {
+    orbitRef.current.update();
+  });
+
+  return (
+    <orbitControls args={[camera, gl.domElement]} autoRotate ref={orbitRef} />
+  );
+};
 
 const App = () => {
-  let keyCount = 0;
-  const boxes = [];
-
-  for (var x = -12; x <= 12; x++) {
-    for (var z = -20; z <= 6; z++) {
-      if (Math.random() >= 0.5) {
-        const randHeight = Math.random() * (2.4 - 1.0) + 1.0;
-        boxes.push(
-          <Box
-            key={`box#${keyCount++}`}
-            initialPosition={[x, 0, z]}
-            color={xBasedColor(Math.abs(x))}
-            randHeight={randHeight}
-          />
-        );
-      }
-    }
-  }
-
   return (
     <div className="App">
       <Canvas
         style={{ touchAction: 'none' }}
-        camera={{ position: [0, 4, 10], lookAt: [0, 0, 0] }}
+        camera={{ position: [0, 10, 16], rotation: [-Math.PI / 8, 0, 0] }}
       >
+        <Controls />
         <ambientLight />
         <spotLight position={[0, 10, 4]} />
-        {boxes}
+        <House />
       </Canvas>
     </div>
   );
