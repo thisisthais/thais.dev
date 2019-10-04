c1f38c2056591b46be885c8469e3ec4dfb095931
Thais Laney <laney.thais@gmail.com>
2019-10-04 15:25:47 -0400

    spiiiin

--- src/Bismuth.js
+++ src/Bismuth.js
@@ -1,54 +1,55 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useRender } from 'react-three-fiber';
 import { a } from 'react-spring/three';
 import * as THREE from 'three';
 
-const OUTER_LENGTH = 1;
-const INNER_LENGTH = 0.9;
+const OUTER_LENGTH = 0.4;
+const INNER_LENGTH = 0.3;
 const HEIGHT = 0.05;
 
 const aBlockVertices = (stretch = 0) => [
-  [-OUTER_LENGTH, HEIGHT, OUTER_LENGTH + stretch], //0
-  [-INNER_LENGTH, HEIGHT, OUTER_LENGTH + stretch], //1
-  [-OUTER_LENGTH, -HEIGHT, OUTER_LENGTH + stretch], //2
-  [-INNER_LENGTH, -HEIGHT, OUTER_LENGTH + stretch], //3
+  [-OUTER_LENGTH - stretch, HEIGHT, OUTER_LENGTH + stretch], //0
+  [-INNER_LENGTH - stretch, HEIGHT, OUTER_LENGTH + stretch], //1
+  [-OUTER_LENGTH - stretch, -HEIGHT, OUTER_LENGTH + stretch], //2
+  [-INNER_LENGTH - stretch, -HEIGHT, OUTER_LENGTH + stretch], //3
   //
-  [-OUTER_LENGTH, HEIGHT, -OUTER_LENGTH - stretch], //4
-  [-INNER_LENGTH, HEIGHT, -OUTER_LENGTH - stretch], //5
-  [-OUTER_LENGTH, -HEIGHT, -OUTER_LENGTH - stretch], //6
-  [-INNER_LENGTH, -HEIGHT, -OUTER_LENGTH - stretch] //7
+  [-OUTER_LENGTH - stretch, HEIGHT, -OUTER_LENGTH - stretch], //4
+  [-INNER_LENGTH - stretch, HEIGHT, -OUTER_LENGTH - stretch], //5
+  [-OUTER_LENGTH - stretch, -HEIGHT, -OUTER_LENGTH - stretch], //6
+  [-INNER_LENGTH - stretch, -HEIGHT, -OUTER_LENGTH - stretch] //7
 ];
 const bBlockVertices = (stretch = 0) => [
-  [-OUTER_LENGTH - stretch, HEIGHT, -INNER_LENGTH], //8
-  [OUTER_LENGTH + stretch, HEIGHT, -INNER_LENGTH], //9
-  [-OUTER_LENGTH - stretch, -HEIGHT, -INNER_LENGTH], //10
-  [OUTER_LENGTH + stretch, -HEIGHT, -INNER_LENGTH], //11
+  [-OUTER_LENGTH - stretch, HEIGHT, -INNER_LENGTH - stretch], //8
+  [OUTER_LENGTH + stretch, HEIGHT, -INNER_LENGTH - stretch], //9
+  [-OUTER_LENGTH - stretch, -HEIGHT, -INNER_LENGTH - stretch], //10
+  [OUTER_LENGTH + stretch, -HEIGHT, -INNER_LENGTH - stretch], //11
   //
-  [-OUTER_LENGTH - stretch, HEIGHT, -OUTER_LENGTH], //12
-  [OUTER_LENGTH + stretch, HEIGHT, -OUTER_LENGTH], //13
-  [-OUTER_LENGTH - stretch, -HEIGHT, -OUTER_LENGTH], //14
-  [OUTER_LENGTH + stretch, -HEIGHT, -OUTER_LENGTH] //15
+  [-OUTER_LENGTH - stretch, HEIGHT, -OUTER_LENGTH - stretch], //12
+  [OUTER_LENGTH + stretch, HEIGHT, -OUTER_LENGTH - stretch], //13
+  [-OUTER_LENGTH - stretch, -HEIGHT, -OUTER_LENGTH - stretch], //14
+  [OUTER_LENGTH + stretch, -HEIGHT, -OUTER_LENGTH - stretch] //15
 ];
 const cBlockVertices = (stretch = 0) => [
-  [INNER_LENGTH, HEIGHT, OUTER_LENGTH + stretch], //16
-  [OUTER_LENGTH, HEIGHT, OUTER_LENGTH + stretch], //17
-  [INNER_LENGTH, -HEIGHT, OUTER_LENGTH + stretch], //18
-  [OUTER_LENGTH, -HEIGHT, OUTER_LENGTH + stretch], //19
+  [INNER_LENGTH + stretch, HEIGHT, OUTER_LENGTH + stretch], //16
+  [OUTER_LENGTH + stretch, HEIGHT, OUTER_LENGTH + stretch], //17
+  [INNER_LENGTH + stretch, -HEIGHT, OUTER_LENGTH + stretch], //18
+  [OUTER_LENGTH + stretch, -HEIGHT, OUTER_LENGTH + stretch], //19
   //
-  [INNER_LENGTH, HEIGHT, -OUTER_LENGTH - stretch], //20
-  [OUTER_LENGTH, HEIGHT, -OUTER_LENGTH - stretch], //21
-  [INNER_LENGTH, -HEIGHT, -OUTER_LENGTH - stretch], //22
-  [OUTER_LENGTH, -HEIGHT, -OUTER_LENGTH - stretch] //23
+  [INNER_LENGTH + stretch, HEIGHT, -OUTER_LENGTH - stretch], //20
+  [OUTER_LENGTH + stretch, HEIGHT, -OUTER_LENGTH - stretch], //21
+  [INNER_LENGTH + stretch, -HEIGHT, -OUTER_LENGTH - stretch], //22
+  [OUTER_LENGTH + stretch, -HEIGHT, -OUTER_LENGTH - stretch] //23
 ];
 const dBlockVertices = (stretch = 0) => [
-  [-OUTER_LENGTH - stretch, HEIGHT, OUTER_LENGTH], //24
-  [OUTER_LENGTH + stretch, HEIGHT, OUTER_LENGTH], //25
-  [-OUTER_LENGTH - stretch, -HEIGHT, OUTER_LENGTH], //26
-  [OUTER_LENGTH + stretch, -HEIGHT, OUTER_LENGTH], //27
+  [-OUTER_LENGTH - stretch, HEIGHT, OUTER_LENGTH + stretch], //24
+  [OUTER_LENGTH + stretch, HEIGHT, OUTER_LENGTH + stretch], //25
+  [-OUTER_LENGTH - stretch, -HEIGHT, OUTER_LENGTH + stretch], //26
+  [OUTER_LENGTH + stretch, -HEIGHT, OUTER_LENGTH + stretch], //27
   //
-  [-OUTER_LENGTH - stretch, HEIGHT, INNER_LENGTH], //28
-  [OUTER_LENGTH + stretch, HEIGHT, INNER_LENGTH], //29
-  [-OUTER_LENGTH - stretch, -HEIGHT, INNER_LENGTH], //30
-  [OUTER_LENGTH + stretch, -HEIGHT, INNER_LENGTH] //31
+  [-OUTER_LENGTH - stretch, HEIGHT, INNER_LENGTH + stretch], //28
+  [OUTER_LENGTH + stretch, HEIGHT, INNER_LENGTH + stretch], //29
+  [-OUTER_LENGTH - stretch, -HEIGHT, INNER_LENGTH + stretch], //30
+  [OUTER_LENGTH + stretch, -HEIGHT, INNER_LENGTH + stretch] //31
 ];
 
 const aBlockFaces = [
@@ -108,10 +109,34 @@ const dBlockFaces = [
   [25, 27, 31]
 ];
 
-const BoxRing = () => {
-  const vertices = [...dBlockVertices(-0.5)];
+const Base = () => {
+  const boxGeo = new THREE.BoxGeometry(OUTER_LENGTH, HEIGHT, OUTER_LENGTH);
+  boxGeo.faces.forEach((face, ndx) => {
+    face.vertexColors = [
+      new THREE.Color().setHSL(ndx / 12, 1, 0.5),
+      new THREE.Color().setHSL(ndx / 12 + 0.1, 1, 0.5),
+      new THREE.Color().setHSL(ndx / 12 + 0.2, 1, 0.5)
+    ];
+  });
+  return (
+    <a.mesh geometry={boxGeo}>
+      <a.meshBasicMaterial
+        attach="material"
+        vertexColors={THREE.VertexColors}
+      />
+    </a.mesh>
+  );
+};
 
-  var faces = [...aBlockFaces];
+const BoxRing = ({ stretch = 0.1, position = [0, 0, 0] }) => {
+  const vertices = [
+    ...aBlockVertices(stretch),
+    ...bBlockVertices(stretch),
+    ...cBlockVertices(stretch),
+    ...dBlockVertices(stretch)
+  ];
+
+  var faces = [...aBlockFaces, ...bBlockFaces, ...cBlockFaces, ...dBlockFaces];
   faces = faces.map(f => new THREE.Face3(...f));
 
   faces.forEach((face, ndx) => {
@@ -122,8 +147,15 @@ const BoxRing = () => {
     ];
   });
 
+  const ringRef = useRef();
+  let theta = 0;
+  useRender(() => {
+    const rotation = (10 / stretch) * THREE.Math.degToRad((theta += 0.3));
+    ringRef.current.rotation.set(rotation, 0, 0);
+  });
+
   return (
-    <a.mesh>
+    <a.mesh ref={ringRef} position={position}>
       <geometry
         attach="geometry"
         vertices={vertices.map(v => new THREE.Vector3(...v))}
@@ -140,7 +172,13 @@ const BoxRing = () => {
 export default () => {
   return (
     <>
-      <BoxRing key="boobi" />
+      <BoxRing stretch={1.5} />
+      <BoxRing stretch={1.25} />
+      <BoxRing stretch={1} />
+      <BoxRing stretch={0.75} />
+      <BoxRing stretch={0.5} />
+      <BoxRing stretch={0.25} />
+      <BoxRing />
     </>
   );
 };
