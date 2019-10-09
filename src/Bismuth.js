1dc79a8dc706d5d813e236bdd131a4bde1465a32
Thais Laney <laney.thais@gmail.com>
2019-10-09 17:21:20 -0400

    recursive tower half working

--- src/Bismuth.js
+++ src/Bismuth.js
@@ -35,30 +35,98 @@ const Base = ({ position = [0, 0, 0] }) => {
   );
 };
 
+const generateTower = (
+  { lengths, position, rotation, height },
+  towerList = []
+) => {
+  if (height <= 1) {
+    return [
+      ...towerList,
+      <Segment position={position} lengths={lengths} rotation={rotation} />
+    ];
+  }
+
+  towerList.push(
+    <Segment position={position} lengths={lengths} rotation={rotation} />
+  );
+  const [x, y, z] = position;
+  return generateTower(
+    {
+      position: [x - 0.05, y + HEIGHT / 2, z + 0.05],
+      rotation,
+      lengths: lengths.map(l => l + 0.1),
+      height: height - 1
+    },
+    towerList
+  );
+};
+
 export default () => {
+  const tower0 = generateTower({
+    lengths: [1, 1, 1, 1],
+    position: [-2, 0, -2],
+    height: 4
+  });
+  const tower1 = generateTower({
+    lengths: [1.2, 1, 1],
+    position: [0, 0, -1.8],
+    height: 4
+  });
+  const tower2 = generateTower({
+    lengths: [1.2, 1, 0.5],
+    position: [2, 0, -1.8],
+    height: 4
+  });
+  const tower3 = generateTower({
+    lengths: [1.2, 1, 1, 0.4],
+    position: [-2, 0, 0],
+    height: 4
+  });
+  const tower4 = generateTower({
+    lengths: [1.2, 1, 0.5, 0.4],
+    position: [0, 0, 0],
+    height: 4
+  });
+  const tower5 = generateTower({
+    lengths: [1.2, 1, 0.7, 0.5, 0.25],
+    position: [2, 0, 0],
+    height: 4
+  });
+  const tower6 = generateTower({
+    lengths: [0.5, 1, 1, 0.4],
+    position: [-2, 0, 1.1],
+    rotation: [0, Math.PI / 2, 0],
+    height: 4
+  });
+  const tower7 = generateTower({
+    lengths: [0.5, 0.5, 1, 1, 0.4],
+    position: [0.5, 0, 1.1],
+    rotation: [0, Math.PI, 0],
+    height: 4
+  });
+  const tower8 = generateTower({
+    lengths: [0.4, 0.3, 0.6, 1, 1, 0.4, 0.3],
+    position: [3, 0, 1],
+    rotation: [0, Math.PI, 0],
+    height: 4
+  });
+  console.log(tower6);
   return (
     <>
-      <Segment position={[-2, 0, -2]} lengths={[1, 1, 1, 1]} />
-      <Segment position={[0, 0, -1.8]} lengths={[1.2, 1, 1]} />
-      <Segment position={[2, 0, -1.8]} lengths={[1.2, 1, 0.5]} />
-      <Segment position={[-2, 0, 0]} lengths={[1.2, 1, 1, 0.4]} />
-      <Segment position={[0, 0, 0]} lengths={[1.2, 1, 0.5, 0.4]} />
-      <Segment position={[2, 0, 0]} lengths={[1.2, 1, 0.7, 0.5, 0.25]} />
-      <Segment
-        position={[-1.7, 0, 1.6]}
-        lengths={[0.5, 1, 1, 0.4]}
-        rotation={[0, Math.PI / 2, 0]}
-      />
-      <Segment
-        position={[0.3, 0, 1.3]}
-        lengths={[0.5, 0.5, 1, 1, 0.4]}
-        rotation={[0, Math.PI, 0]}
-      />
-      <Segment
+      {tower0}
+      {tower1}
+      {tower2}
+      {tower3}
+      {tower4}
+      {tower5}
+      {tower6}
+      {tower7}
+      {tower8}
+      {/* <Segment
         position={[2, 0, 1]}
         lengths={[0.4, 0.3, 0.6, 1, 1, 0.4, 0.3]}
         rotation={[0, Math.PI, 0]}
-      />
+      /> */}
     </>
   );
 };
