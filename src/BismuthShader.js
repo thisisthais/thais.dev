8ff9823aede661e1f9c3e7e3ec3edf776ccc3d90
Thais Laney <laney.thais@gmail.com>
2019-11-19 17:55:24 -0500

    good enough bismuth shader for now

--- src/BismuthShader.js
+++ src/BismuthShader.js
@@ -37,26 +37,13 @@ const BismuthShader = {
     uniform float iTime;
     uniform vec3 iResolution;
 
-    // --- Spectral Zucconi --------------------------------------------
-    // By Alan Zucconi
-    // Based on GPU Gems: https://developer.nvidia.com/sites/all/modules/custom/gpugems/books/GPUGems/gpugems_ch08.html
-    // But with values optimised to match as close as possible the visible spectrum
-    // Fits this: https://commons.wikimedia.org/wiki/File:Linear_visible_spectrum.svg
-    // With weighter MSE (RGB weights: 0.3, 0.59, 0.11)
-    vec3 bump3y (vec3 x, vec3 yoffset) {
-      vec3 y = vec3(1.,1.,1.) - x * x;
-      y = saturate(y-yoffset);
+    vec3 bump3y(in vec3 x, in vec3 yoffset) {
+      vec3 y = vec3(1.0) - x * x;
+      y = clamp(y - yoffset, 0.0, 1.9);
       return y;
     }
 
-    // --- Spectral Zucconi 6 -----------------------------------------
-    // Based on GPU Gems
-    // Optimised by Alan Zucconi
-    vec3 spectral_zucconi6 (float w) {
-      // w: [400, 700]
-      // x: [0,   1]
-      float x = saturate((w - 400.0)/ 300.0);
-
+    vec3 spectral_zucconi6(in float x) {
       const vec3 c1 = vec3(3.54585104, 2.93225262, 2.41593945);
       const vec3 x1 = vec3(0.69549072, 0.49228336, 0.27699880);
       const vec3 y1 = vec3(0.02312639, 0.15225084, 0.52607955);
@@ -67,27 +54,29 @@ const BismuthShader = {
 
       return
         bump3y(c1 * (x - x1), y1) +
-        bump3y(c2 * (x - x2), y2) ;
+        bump3y(c2 * (x - x2), y2);
     }
 
     void main() {
       vec3 viewVector = normalize(cameraPosition - Position);
       float viewDirection = acos(dot(viewVector, Normal)/length(viewVector)*length(Normal));
-    
+
       vec3 lightVector = normalize(LightPosition.xyz - Position);
       float lightDirection = acos(dot(lightVector, Normal)/length(viewVector)*length(Normal));
-    
-      vec3 initialColor = spectral_zucconi6(Position.x + Position.y + Position.z)/3.;
-      
+
+      float w = abs(viewVector.x)*300. + 400.;
+      //vec3 initialColor = spectral_zucconi6(w);
+      vec3 initialColor = spectral_zucconi6((viewVector.x + viewVector.y + viewVector.z)/2.);
+
       vec3 color = vec3(0.);
       float gapDistance =1.25;
       for (int n = 1; n <= 8; n++) {
         float wavelength = abs(sin(lightDirection) - sin(viewDirection))*gapDistance / float(n);
         color += spectral_zucconi6(wavelength);
       }
-      color.x *= 1.5;
+      color *= 0.5;
       initialColor += color;
-    
+
       gl_FragColor = vec4(initialColor, 1.0);
     }
   `
