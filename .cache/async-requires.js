0e84b9fcd5601a8a7c5f08f598ab3b5761413711
thisisthais <laney.thais@gmail.com>
2020-07-25 21:44:43 -0400

    new route for bismuth

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,6 +4,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-bismuth-js": () => import("./../src/pages/bismuth.js" /* webpackChunkName: "component---src-pages-bismuth-js" */),
   "component---src-pages-blog-first-en-mdx": () => import("./../src/pages/blog/first.en.mdx" /* webpackChunkName: "component---src-pages-blog-first-en-mdx" */),
   "component---src-pages-blog-first-pt-mdx": () => import("./../src/pages/blog/first.pt.mdx" /* webpackChunkName: "component---src-pages-blog-first-pt-mdx" */),
   "component---src-pages-index-en-js": () => import("./../src/pages/index.en.js" /* webpackChunkName: "component---src-pages-index-en-js" */),
---
+++ src/pages/bismuth.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BismuthPage() {
+  return (
+    <div>
+      <h1>pretty rocks incoming</h1>
+    </div>
+  );
+}
