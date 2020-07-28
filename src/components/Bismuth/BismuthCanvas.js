5839ae4db957c5d1e9b7e99799b313c598cc37df
thisisthais <laney.thais@gmail.com>
2020-07-28 16:46:46 -0400

    better working, no click jump, still ugly CSS

--- src/components/Bismuth/BismuthCanvas.js
+++ src/components/Bismuth/BismuthCanvas.js
@@ -5,6 +5,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Bismuth from './Bismuth';
 import { Button, Paper, Slider } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import polyfill from '@juggle/resize-observer';
 
 import './bismuthCanvas.css';
 
@@ -29,26 +30,15 @@ const MySlider = withStyles({
 
 const MyButton = withStyles({
   root: {
-    color: '#ba6ca7',
-    borderColor: '#ba6ca7',
+    color: '#a46fd9',
+    borderColor: '#a46fd9',
+    marginTop: '-0.5em',
+    marginBottom: '0.5em',
     '&:hover': {
       backgroundColor: 'rgba(138, 84, 171, 0.2)',
     },
   },
 })(Button);
-
-const MyPaper = withStyles({
-  root: {
-    alignItems: 'baseline',
-    boxShadow:
-      '2px 3px 1px -1px rgba(134, 88, 184, 0.2), 2px 2px 2px 0px rgba(134, 88, 184, 0.2), 2px 2px 4px 0px rgba(134, 88, 184, 0.2)',
-    display: 'flex',
-    flexDirection: 'row',
-    padding: '0.5em 2em 0.5em 2em',
-    margin: '1em',
-    maxHeight: '40%',
-  },
-})(Paper);
 
 const BismuthCanvas = () => {
   // cleam this up later
@@ -59,82 +49,87 @@ const BismuthCanvas = () => {
   const [gapDistance, setGapDistance] = useState(1.25);
 
   return (
-    <div className="bismuth">
-      <MyPaper>
-        <div className="textContainer">
-          <h5>Play around with some input values :)</h5>
-          <h5>A new bismuth will automatically generate on any change.</h5>
-          <h5>
-            You can also generate new bismuths while keeping the same inputs by
-            using the button.
-          </h5>
-        </div>
-        <div className="slidersContainer">
-          <h4>Base Tower Height Range</h4>
-          <MySlider
-            getAriaLabel={() => 'Base Tower Height Range'}
-            defaultValue={baseTowerHeightRange}
-            marks={true}
-            min={1}
-            max={30}
-            onChange={(_, value) => setBaseTowerHeightRange(value)}
-            valueLabelDisplay="auto"
-          />
-          <h4>Number of Layers</h4>
-          <MySlider
-            aria-label="Number of Layers"
-            defaultValue={numLayers}
-            marks={true}
-            min={3}
-            max={10}
-            onChange={(_, value) => setNumLayers(value)}
-            valueLabelDisplay="auto"
-          />
-          <h4>Tower Height Range</h4>
-          <MySlider
-            getAriaLabel={() => 'Tower Height Range'}
-            defaultValue={towerHeightRange}
-            marks={true}
-            min={5}
-            max={40}
-            onChange={(_, value) => setTowerHeightRange(value)}
-            valueLabelDisplay="auto"
-          />
-          <h4>Iridescence Gap Distance</h4>
-          <MySlider
-            aria-label="Iridescence Gap Distance"
-            defaultValue={gapDistance}
-            min={0.01}
-            max={5.0}
-            step={0.01}
-            onChange={(_, value) => setGapDistance(value)}
-            valueLabelDisplay="auto"
-          />
+    <div className="bismuthContainer">
+      <div className="sidebar">
+        <div className="sidebarContent">
+          <div className="sidebarTitle">
+            <span role="img" aria-label="sparkle emoji">
+              ✨✨✨
+            </span>
+            <h2>Bismuth Generator</h2>
+            <span role="img" aria-label="sparkle emoji">
+              ✨✨✨
+            </span>
+          </div>
+          <div className="slider1">
+            <h4>Base Tower Height Range</h4>
+            <MySlider
+              getAriaLabel={() => 'Base Tower Height Range'}
+              defaultValue={baseTowerHeightRange}
+              marks={true}
+              min={1}
+              max={30}
+              onChange={(_, value) => setBaseTowerHeightRange(value)}
+              valueLabelDisplay="auto"
+            />
+          </div>
+          <div className="slider2">
+            <h4>Number of Layers</h4>
+            <MySlider
+              aria-label="Number of Layers"
+              defaultValue={numLayers}
+              marks={true}
+              min={3}
+              max={10}
+              onChange={(_, value) => setNumLayers(value)}
+              valueLabelDisplay="auto"
+            />
+          </div>
+          <div className="slider3">
+            <h4>Tower Height Range</h4>
+            <MySlider
+              getAriaLabel={() => 'Tower Height Range'}
+              defaultValue={towerHeightRange}
+              marks={true}
+              min={5}
+              max={40}
+              onChange={(_, value) => setTowerHeightRange(value)}
+              valueLabelDisplay="auto"
+            />
+          </div>
+          <div className="slider4">
+            <h4>Iridescence Gap Distance</h4>
+            <MySlider
+              aria-label="Iridescence Gap Distance"
+              defaultValue={gapDistance}
+              min={0.01}
+              max={5.0}
+              step={0.01}
+              onChange={(_, value) => setGapDistance(value)}
+              valueLabelDisplay="auto"
+            />
+          </div>
           <MyButton
+            className="button"
             variant="outlined"
             size="small"
             onClick={() => setRegenBismuth(!regenBismuth)}
           >
-            Gen new bismuth
+            Regenerate
           </MyButton>
         </div>
-      </MyPaper>
+      </div>
       <Canvas
+        resize={{ polyfill }}
         id="myCanvas"
         style={{ touchAction: 'none' }}
         camera={{
-          position: [5, 9, 5],
+          position: [-3, 5, -3],
         }}
       >
         <Controls />
-        {/* <ambientLight /> */}
-        {/* <spotLight position={[0, 10, 4]} /> */}
         <axesHelper args={[5]} />
         <Bismuth />
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-          <meshStandardMaterial attach="material" color={'hotpink'} />
-        </mesh>
       </Canvas>
     </div>
   );
