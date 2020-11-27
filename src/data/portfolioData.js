5291ed7b9003d51234b0cba688fd629e92c30240
thisisthais <laney.thais@gmail.com>
2020-11-27 17:50:18 -0500

    project card spaces images, title, desc, and tags well

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,12 +2,36 @@ export default {
   doNothing: {
     title: 'Please, Do Nothing',
     shortDesc:
-      "Inspired by Jenny Odell's How To Do Nothing and my interest in playing with emergent web tech, in this case in-browser eye-tracking ML, 3D graphics, and GLSL.",
-    thumbnail: 'src/images/doNothing.png',
+      "Inspired by Jenny Odell's How To Do Nothing and my interest in emergent web tech, in this case in-browser eye-tracking ML, 3D graphics, and GLSL.",
+    thumbnail: 'src/images/doNothing.gif',
     tags: ['#ThreeJs', '#GLSL', '#RecurseCenter', '#browserML'],
   },
-  bismuth: {},
-  shaderSketches: {},
+  bismuth: {
+    title: 'Generative Bismuth',
+    shortDesc:
+      'A generative system for making the most beautiful mineral, bismuth. Prototype for future series exploring crystal growth patterns.',
+    thumbnail: 'src/images/bismuth.gif',
+    tags: [
+      '#generativeArt',
+      '#ThreeJs',
+      '#GLSL',
+      '#RecurseCenter',
+      '#react-three-fiber',
+    ],
+  },
+  shaderSketches: {
+    title: 'Shader Sketches',
+    shortDesc:
+      'Daily sketches exploring GLSL, done while working through The Book of Shaders',
+    thumbnail: 'src/images/shaderSketches.gif',
+    tags: [
+      '#artPractice',
+      '#generativeArt',
+      '#GLSL',
+      '#RecurseCenter',
+      '#TheBookOfShaders',
+    ],
+  },
   dynamicland: {},
   mujic: {},
   girlsWhoCode: {},
--- src/pages/portfolio.en.js
+++ src/pages/portfolio.en.js
@@ -3,22 +3,30 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import Layout from '../components/layout.js';
 import doNothingThumb from '../images/doNothing.gif';
+import bismuthThumb from '../images/bismuth.gif';
+import shaderSketchesThumb from '../images/shaderSketches.gif';
 import portfolioData from '../data/portfolioData';
 
 import '../components/portfolio.css';
 
-const ProjectCard = ({}) => {
+const PROJECT_TO_THUMBNAIL = {
+  doNothing: doNothingThumb,
+  bismuth: bismuthThumb,
+  shaderSketches: shaderSketchesThumb,
+};
+
+const ProjectCard = ({ project }) => {
   return (
     <div className="projectCard">
-      <img src={doNothingThumb} />
+      <img src={PROJECT_TO_THUMBNAIL[project]} />
       <h3>
-        <FormattedMessage id="portfolioData.doNothing.title" />
+        <FormattedMessage id={`portfolioData.${project}.title`} />
       </h3>
       <p>
-        <FormattedMessage id="portfolioData.doNothing.shortDesc" />
+        <FormattedMessage id={`portfolioData.${project}.shortDesc`} />
       </p>
       <marquee scrollamount="3">
-        {portfolioData.doNothing.tags.map((t) => (
+        {(portfolioData[project].tags || []).map((t) => (
           <a href="">{t}</a>
         ))}
       </marquee>
@@ -30,8 +38,8 @@ const ENPortfolioPage = (props) => {
   return (
     <Layout location={props.location}>
       <div className="projectsContainer">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((t) => (
-          <ProjectCard key={t} text={t} />
+        {Object.keys(portfolioData).map((project, i) => (
+          <ProjectCard key={i} project={project} />
         ))}
       </div>
     </Layout>
