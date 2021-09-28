344221caf06336005406c5659e2f3ddd238353d5
thisisthais <laney.thais@gmail.com>
2021-09-28 14:50:35 -0400

    GLTFExporter import working

--- src/pages/selfToken.js
+++ src/pages/selfToken.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
+import React, { useRef, useState, useEffect } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
+
+function Box(props) {
+  const exporter = new GLTFExporter();
+  // This reference will give us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => {
+        setActive(!active);
+        exporter.parse(
+          ref.current,
+          function (gltf) {
+            downloadJSON(gltf, 'selfToken.gltf');
+          },
+          {}
+        );
+      }}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
 
 export default function SelfToken() {
   return (
-    <div>
-      <h1>HI</h1>
-    </div>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
+}
+
+function downloadJSON(json, filename) {
+  saveString(JSON.stringify(json), filename);
+}
+
+function saveString(text, filename) {
+  save(new Blob([text], { type: 'model/gltf+json' }), filename);
+}
+
+var link = document.createElement('a');
+link.style.display = 'none';
+document.body.appendChild(link); // Firefox workaround, see #6594
+
+function save(blob, filename) {
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+
+  // URL.revokeObjectURL( url ); breaks Firefox...
 }
